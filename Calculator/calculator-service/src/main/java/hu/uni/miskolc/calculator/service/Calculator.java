c0d4d5d462ad51ed8a966ff5da4a17d79f233e11
Lili-PC <Lili-PC@192.168.2.13>
2017-03-10 08:29:22 +0100

    service

---
+++ Calculator/calculator-service/src/main/java/hu/uni/miskolc/calculator/service/Calculator.java
@@ -0,0 +1,8 @@
+package hu.uni.miskolc.calculator.service;
+
+public interface Calculator {
+	public double add(double a, double b);
+	public double subtraction(double a, double b);
+	public double multiplication(double a, double b);
+	public double division(double a, double b);
+}
